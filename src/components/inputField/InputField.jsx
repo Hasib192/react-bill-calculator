7463bfe6e24d6976ea9aab7dbf17eb6fa0104812
Hasib <hasibhosen7612@gmail.com>
2023-08-03 15:33:19 +0600

    fix/overflowing outside problem fixed

--- src/components/inputField/InputField.jsx
+++ src/components/inputField/InputField.jsx
@@ -1,22 +1,32 @@
+import { useState } from "react";
 const InputField = ({ label, className, placeholder, min, handleInputChange }) => {
+  const [showWarning, setShowWarning] = useState(false);
   const handleKeyPress = (e) => {
     // Prevent typing of "-" and "+"
     if (e.key === "-" || e.key === "+") {
       e.preventDefault();
     }
+  };
 
-    // Check length and prevent input if it exceeds 7 digits
-    let str = " " + e.target.value;
-    if (str.length > 7) {
-      alert("Input cannot exceed 7 digits!");
-      e.preventDefault();
+  const handleInput = (event) => {
+    const inputText = event.target.value;
+    if (inputText.length > 7) {
+      event.target.value = inputText.slice(0, 7);
+      setShowWarning(true);
+    } else {
+      setShowWarning(false);
     }
   };
 
   return (
     <div className={className}>
       <label>{label}</label>
-      <input type="number" min={min} step="1" placeholder={placeholder} maxLength={6} onKeyDown={handleKeyPress} onChange={handleInputChange} />
+      <input type="number" min={min} step="1" placeholder={placeholder} maxLength={6} onKeyDown={handleKeyPress} onInput={handleInput} onChange={handleInputChange} />
+      {showWarning && (
+        <small style={{ color: "red", fontSize: 12 }}>
+          <em>Amount cannot exceed 7 digits</em>
+        </small>
+      )}
     </div>
   );
 };
